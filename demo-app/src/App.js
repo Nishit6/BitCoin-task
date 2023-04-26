4a53e6d7f1e29fe85613fe5c5f3ef2298468af8b
Nishit Gaur <nishitgaur6@gmail.com>
2023-04-26 16:05:57 +0530

    bitcoin v1

---
+++ demo-app/src/App.js
@@ -0,0 +1,17 @@
+
+import { Fragment } from 'react';
+
+import './App.css';
+import Bitcoin from './Components/Bitcoin';
+
+
+
+function App() {
+  return (
+   <Fragment>
+   <Bitcoin/>
+   </Fragment>
+  );
+}
+
+export default App;
---
+++ demo-app/src/Components/Bitcoin.js
@@ -0,0 +1,58 @@
+import React, {useState,Fragment}from 'react'
+import {Card, Button } from 'react-bootstrap'
+import './Bitcoin.css'
+
+
+export default function Bitcoin() {
+
+    const [inputVal, setInputVal] = useState('')
+    const [blueColor, setBlueColor] = useState(true)
+    const [bitCoinPrice, setBitCoinPrice] = useState('')
+    const [previousPrice, setPreviousPrice] = useState([])
+    const [showPreviousPrice, setShowPreviousPrice] = useState(false)
+
+const inputHandler = (e)=>{
+    setInputVal(e.target.value)
+    setPreviousPrice({...previousPrice, bitCoinPrice})
+}
+
+    const buttonHandler = ()=>{
+        if(inputVal.length < 0){
+return
+        }
+      setBitCoinPrice(inputVal)
+      setInputVal('')
+    }
+
+const colorChangeHandler = () =>{
+    setBlueColor(!blueColor)
+}
+
+const prevPriceHandler = ()=>{
+ setShowPreviousPrice(!previousPrice)
+}
+
+
+  return (
+    <Fragment>
+        <h2>BitCoin Price</h2>
+        <div className='container'>
+      
+    <Card style={{ width: '18rem' }}>
+    <Card.Body>
+      <Card.Title className={blueColor ? 'title-blue' : 'title-purple'} >{bitCoinPrice.length > 0 ? "current price: " + bitCoinPrice : 'please add price'}</Card.Title>
+      <Card.Title >{showPreviousPrice && "previous price: " + previousPrice }</Card.Title>
+      <input value={inputVal} type='text' required={true} onChange={inputHandler}/>
+      {bitCoinPrice.length > 0 ?  <Button onClick={buttonHandler} className='mb-3 mt-3' variant="success">Refresh Bitcoin Price</Button> :    <Button onClick={buttonHandler} className='mb-3 mt-3' variant="warning">get Bitcoin Price</Button> }
+
+      <Button onClick={colorChangeHandler} className='mb-3' variant="secondary">change color</Button>
+
+      <Button onClick={prevPriceHandler} className='mb-3 mt-3' variant="info">get Previous price</Button>
+   
+    </Card.Body>
+  </Card>
+  </div>
+  </Fragment>
+  )
+  
+}
